c81d20e0388f0515b134391672707c8343dba7a1
lucac-zilliqa <137715422+lucac-zilliqa@users.noreply.github.com>
2023-08-07 14:38:29 +0100

    Add blocks page (#12)

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { RuntimeContext, useRuntime } from "./useRuntime";
 import { ChainInfoContext, useChainInfoFromMetadataFile } from "./useChainInfo";
 
 const Block = lazy(() => import("./execution/Block"));
+const BlockList = lazy(() => import("./execution/BlockList"));
 const BlockTransactions = lazy(() => import("./execution/BlockTransactions"));
 const Address = lazy(() => import("./execution/Address"));
 const Transaction = lazy(() => import("./execution/Transaction"));
@@ -25,6 +26,7 @@ const Validator = lazy(() => import("./consensus/Validator"));
 const London = lazy(() => import("./special/london/London"));
 const Faucets = lazy(() => import("./Faucets"));
 const PageNotFound = lazy(() => import("./PageNotFound"));
+
 
 const App = () => {
   const runtime = useRuntime();
@@ -55,6 +57,9 @@ const App = () => {
                     <Route
                       path="block/:blockNumberOrHash"
                       element={<Block />}
+                    />
+                    <Route
+                      path="blocklist" element={ <BlockList/>}
                     />
                     <Route
                       path="block/:blockNumber/txs"
