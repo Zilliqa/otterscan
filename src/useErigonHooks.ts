1d836094cc1300281c5e1743c396cdeb113f5b62
lucac-zilliqa <lucachoteborsky@zilliqa.com>
2023-08-09 10:49:17 +0100

    Changed Runtime Context to contain Zilliqa object

--- src/useErigonHooks.ts
+++ src/useErigonHooks.ts
@@ -24,6 +24,8 @@ import {
   TokenMeta,
 } from "./types";
 import erc20 from "./erc20.json";
+import { DsBlockObj } from '@zilliqa-js/core/dist/types/src/types'
+import { Zilliqa } from "@zilliqa-js/zilliqa";
 
 const TRANSFER_TOPIC =
   "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
@@ -178,6 +180,50 @@ export const useBlockData = (
     return { data: undefined, isLoading: false };
   }
   return { data, isLoading };
+};
+
+const dsBlockDataFetcher: Fetcher<
+    DsBlockObj | null,
+  [Zilliqa, string]
+> = async ([zilliqa, blockNum]) => {
+  const response = await zilliqa.blockchain.getDSBlock(
+      parseInt(blockNum, 10)
+      );
+  if (response.error !== undefined) throw new Error(response.error.message);
+  if (response.result.header.BlockNum !== blockNum)
+    throw new Error("Invalid DS Block Number");
+  return response.result as DsBlockObj;
+};
+
+export const useDSBlockData = (
+  zilliqa : Zilliqa | undefined,
+  blockNumber: number | undefined,
+  pageNumber: number,
+  pageSize: number
+): { data: (DsBlockObj | null)[] | undefined; isLoading: boolean } => {
+  const startBlockNum : number | undefined = blockNumber ? blockNumber - ( pageSize * pageNumber) : undefined;
+
+  // This function is used by SWR to get the key which we pass to the fetcher function
+  // It also searches the cache for the presence of this key and if found returns the 
+  // cached value. The pageSize differenciates the cache between components so that different components
+  // do not display incorrect number of displays
+  const getKey = (pageIndex : number) 
+  : [ Zilliqa, string, number] | null =>  {
+    if((zilliqa == undefined || startBlockNum == undefined) 
+    || (startBlockNum - pageIndex < 0)) return null;
+
+    return [zilliqa, (startBlockNum - pageIndex).toString(), pageSize]
+  }
+
+  // Calls the fetcher to fetch the most recent pageNumber of blocks in parallel
+  const { data, error, isLoading, isValidating } = useSWRInfinite(getKey,
+    dsBlockDataFetcher,
+    { keepPreviousData: true, revalidateFirstPage : false, initialSize : pageSize, parallel : true }
+  );
+  if (error) {
+    return { data: undefined, isLoading: false };
+  }
+  return { data, isLoading :  isLoading || isValidating};
 };
 
 export const useRecentBlocks = (
