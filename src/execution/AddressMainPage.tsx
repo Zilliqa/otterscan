928122a4bb4f519743730da431c132223e2e7f1c
Willian Mitsuda <wmitsuda@gmail.com>
2023-12-24 07:05:25 -0300

    Reactivate blocks rewarded support (#1636)
    * Revert "Disable tab/route for now; feature will be postponed to the next release (#1451)"
    
    This reverts commit ff4f1e073fcada4c042b0df72f3bafe283e6adc3.
    
    * Rename method
    
    * Reactivate blocks rewarded support; added block fees column

--- src/execution/AddressMainPage.tsx
+++ src/execution/AddressMainPage.tsx
@@ -27,6 +27,7 @@ import AddressSubtitle from "./address/AddressSubtitle";
 import AddressTokens from "./address/AddressTokens";
 import AddressTransactionResults from "./address/AddressTransactionResults";
 import AddressWithdrawals from "./address/AddressWithdrawals";
+import BlocksRewarded from "./address/BlocksRewarded";
 import Contracts from "./address/Contracts";
 import ReadContract from "./address/contract/ReadContract";
 import { fromBech32Address } from '@zilliqa-js/crypto'
@@ -111,6 +112,9 @@ const AddressMainPage: React.FC<AddressMainPageProps> = () => {
                     </NavTab>
                     <NavTab href={`/address/${addressOrName}/withdrawals`}>
                       Withdrawals
+                    </NavTab>
+                    <NavTab href={`/address/${addressOrName}/blocksRewarded`}>
+                      Blocks Rewarded
                     </NavTab>
                   </>
                 )}
@@ -200,6 +204,12 @@ const AddressMainPage: React.FC<AddressMainPageProps> = () => {
                         path="withdrawals"
                         element={
                           <AddressWithdrawals address={checksummedAddress} />
+                        }
+                      />
+                      <Route
+                        path="blocksRewarded"
+                        element={
+                          <BlocksRewarded address={checksummedAddress} />
                         }
                       />
                     </>
