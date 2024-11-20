c5d5d3b5836d0bde28080d325278e547b0fbf1c3
Richard Watts <108257153+rrw-zilliqa@users.noreply.github.com>
2024-11-20 11:43:14 +0000

    (feat) Multi-column display for block list; ability to display proposer (#66)

--- src/search/BlockResultHeader.tsx
+++ src/search/BlockResultHeader.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { EmptyBlocksDisplay } from "./useEmptyBlocksToggler";
 import { FeeDisplay } from "./useFeeToggler";
+import { MultiColumnDisplay } from "./useMultiColumnDisplayToggler";
 
 export type ResultHeaderProps = {
   feeDisplay: FeeDisplay;
   emptyBlocksDisplay: EmptyBlocksDisplay;
   feeDisplayToggler: () => void;
   emptyBlocksDisplayToggler: () => void;
+  multiColumnDisplay: MultiColumnDisplay;
+  multiColumnDisplayToggler: () => void;
 };
 
 const BlockResultHeader: React.FC<ResultHeaderProps> = ({
@@ -14,8 +17,10 @@ const BlockResultHeader: React.FC<ResultHeaderProps> = ({
   feeDisplayToggler,
   emptyBlocksDisplay,
   emptyBlocksDisplayToggler,
+  multiColumnDisplay,
+  multiColumnDisplayToggler,
 }) => (
-  <div className="grid grid-cols-9 gap-x-1 border-t border-b border-gray-200 bg-gray-100 px-2 py-2 text-sm font-bold text-gray-500">
+  <div className="grid grid-cols-10 gap-x-1 border-t border-b border-gray-200 bg-gray-100 px-2 py-2 text-sm font-bold text-gray-500">
     <div>Height</div>
     <div>
       <button
@@ -28,7 +33,16 @@ const BlockResultHeader: React.FC<ResultHeaderProps> = ({
           "Txns [non-empty-blks]"}
       </button>
     </div>
-    <div className="col-span-4">Block Hash</div>
+    <div className="col-span-3">Block Hash</div>
+    <div className="col-span-2">
+      <button
+        className="text-link-blue hover:text-link-blue-hover"
+        onClick={multiColumnDisplayToggler}
+      >
+        {multiColumnDisplay == MultiColumnDisplay.SHOW_PARENT && "Parent"}
+        {multiColumnDisplay == MultiColumnDisplay.SHOW_PROPOSER && "Proposer"}
+      </button>
+    </div>
     <div>
       <button
         className="text-link-blue hover:text-link-blue-hover"
